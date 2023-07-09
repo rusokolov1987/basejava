e74185ec49ee1f4dd979638885f8e5dd7dc1418d
rusokolov <rusokolov4@gmail.com>
2023-07-09 23:25:21 +0300

    feat: add functionality get() into ArrayStorage.java

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -23,6 +23,12 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
+        int size = size();
+        for (int i = 0; i < size; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 

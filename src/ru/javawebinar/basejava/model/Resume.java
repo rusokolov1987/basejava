949759ba0cbc8285044175eed3124a4b9af4916b
rusokolov <rusokolov4@gmail.com>
2023-07-26 22:55:53 +0300

    refact: refacting code

--- src/ru/javawebinar/basejava/model/Resume.java
+++ src/ru/javawebinar/basejava/model/Resume.java
@@ -1,10 +1,11 @@
 package ru.javawebinar.basejava.model;
 
+import java.util.Objects;
+
 /**
  * Initial resume class
  */
 public class Resume {
-
     // Unique identifier
     private String uuid;
 
@@ -19,5 +20,18 @@ public class Resume {
     @Override
     public String toString() {
         return uuid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Resume resume = (Resume) o;
+        return uuid.equals(resume.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return uuid.hashCode();
     }
 }

1ad53fa01fe4a80392cab983608e28bf1da475ad
rusokolov <rusokolov4@gmail.com>
2023-07-26 22:47:39 +0300

    feat: refactore code

--- src/ru/javawebinar/basejava/MainTestArrayStorage.java
+++ src/ru/javawebinar/basejava/MainTestArrayStorage.java
@@ -1,7 +1,7 @@
-package com.urise.webapp;
+package ru.javawebinar.basejava;
 
-import com.urise.webapp.model.Resume;
-import com.urise.webapp.storage.ArrayStorage;
+import ru.javawebinar.basejava.model.Resume;
+import ru.javawebinar.basejava.storage.ArrayStorage;
 
 /**
  * Test for your ArrayStorage implementation
